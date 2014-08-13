11a46fcd0b166ca4ee9cbdcfcc2e727d8c897c4d
Alec Bellati <alec.bellati@student.adelaide.edu.au>
2014-08-13 16:21:15 +0930

    Rewrite the Individual class.

--- assignment1/trunk/Individual.java
+++ assignment1/trunk/Individual.java
@@ -3,76 +3,90 @@ import java.util.Random;
 
 public class Individual{
 
-	/** TSP graph generated from XML parser */
-	private double[][] TSPGraph;
+	/** Contains the solution associated with this Individual */
+	private City[] solution;
 
-	/** Contains the cities with their associated weights
-	Should contain ame information as above (TSPGraph) */
-	private City[] cities;
+	/** The fitness value of this solution*/
+	private double fitness;
 
 	/*
-	* Takes given TSP Graph and assigns to local variable
-	* Initialises solution array list
-	*/
-	public Individual(double[][] TSPGraph, City[] cities){
-		this.TSPGraph = TSPGraph;
-		this.cities = cities;
+	 * Takes given TSP Graph and assigns to local variable
+	 * Initialises solution array list
+	 * @param TSPGraph - The TSP graph that is being used
+	 * @param solution - The solution for this Individual
+	 * @param preserve - A flag to indicate whether the order solution should be preserved
+	 */
+	public Individual(double[][] TSPGraph, City[] solution, boolean preserve){
+		this.solution = solution;
+		
+		// Shuffle the solution if it does not need to be preserved
+		if (!preserve){
+			shuffleArray();
+		}
+		
+		// Calculate the cost of this solution
+		calculate_cost(TSPGraph);
 	}
 
 	/*
 	* Gets the basic solution set
-	* Returns a single array of cities
-	* Should not generate any errors
+	* @return An array of cities
 	*/
-	public City[] get_single_solution(){
-		shuffleArray();
-		return cities.clone();
+	public City[] get_solution(){
+		return solution;
 	}
 
 	/**
-	* Given a city, return its cost
-	* Returns a double, does not generate errors
+	* Get the number of cities in the solution set
+	* @return The number of cities in the solution
 	*/
-	public double get_cost(City[] single_city){
-		double total = 0;
-		for(int i = 0; i < single_city.length; i++){
-			City curr_city = single_city[i];
+	public int get_num_cities(){
+		return solution.length;
+	}
+
+	/**
+	* Get the fitness of the solution
+	* @return - The fitness of this solution
+	*/
+	public double get_fitness(){
+		return fitness;
+	}
+
+	/**
+	* Given a city, calculate its fitness
+	* @param TSPGraph - The TSP graph being used
+	*/
+	private void calculate_fitness(double[][] TSPGraph){
+		double fitness = 0;
+		for(int i = 0; i < solution.length; i++){
+			City curr_city = solution[i];
 			City next_city;
 			double cost_to_node = 0;
 
-			if(i != single_city.length-1){
-				next_city = single_city[i+1];
+			if(i != solution.length-1){
+				next_city = solution[i+1];
 				cost_to_node = TSPGraph[curr_city.get_node_num()][next_city.get_node_num()];
 			}else{ //return to start
-				next_city = single_city[0];
+				next_city = solution[0];
 				cost_to_node = TSPGraph[curr_city.get_node_num()][next_city.get_node_num()];
 			}
-			total += cost_to_node;
+			fitness += cost_to_node;
 		}
-
-		return total;
 	}
-
-	/**
-	* Return the number of cities in the solution set
-	*/
-	public int get_num_cities(){
-		return cities.length;
-	}
-
+	
 	/**
 	* Implementing Fisher–Yates shuffle
 	* Randomly shuffle the cities array to form a random solution
 	*/
   	private void shuffleArray(){
     	Random rnd = new Random();
-    	for (int i = cities.length - 1; i > 0; i--){
+    	for (int i = solution.length - 1; i > 0; i--){
       		//int index = rnd.nextInt(i + 1);
       		int index = rnd.nextInt(cities.length-1);
       		// Simple swap
-      		City temp = cities[index];
-      		cities[index] = cities[i];
-      		cities[i] = temp;
+      		City temp = solution[index];
+      		solution[index] = solution[i];
+      		solution[i] = temp;
     	}
   	}
 }
