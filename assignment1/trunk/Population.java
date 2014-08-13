eca183cf28f737d12877762cdf06f06fdd38bdca
Alec Bellati <alec.bellati@student.adelaide.edu.au>
2014-08-13 16:50:17 +0930

    Rewrite the Population class

--- assignment1/trunk/Population.java
+++ assignment1/trunk/Population.java
@@ -1,26 +1,90 @@
 public class Population{
 	
-	/** Individual contains logic for generating a single solution */
-	public Individual single_sol;
-
+	/** TSP graph generated from XML parser */
+	private double[][] TSPGraph;
+	
+	/** Set of solutions */
+	private Individual[] solution_set;
+	
+	/** Size of population.
+	Not necessarily the actual size,
+	but what the size of the selected set should be */
+	private int population_size;
+	
 	/**
-	* Constructor takes in initialised individual object
-	* Used to extract multiple single solutions to form a population
+	* Constructor takes in a TSP graph
+	* Used to initialise multiple single solutions to form a population
 	*/
-	public Population(Individual single_sol){
-		this.single_sol = single_sol;
+	public Population(double[][] TSPGraph, City[] cities, int population_size){
+		this.TSPGraph = TSPGraph;
+		this.population_size = population_size;
+		
+		create_solution_set(cities);
 	}
 
 	/*
-	* Gets a basic set of solutions - specified by the user
-	* Returns a City array of solutions
-	* Should not generate any errors
+	* Gets the TSP graph
+	* @return - A 2D double array
 	*/
-	public City[][] get_solution_set(int num_solutions){
-		City[][] solution_set = new City[num_solutions][single_sol.get_num_cities()];
-		for(int i = 0; i < num_solutions; i++){
-			solution_set[i] = single_sol.get_single_solution();
-		}
+	public double[][] get_TSPGraph(){
+		return TSPGraph;
+	}
+	
+	/*
+	* Gets a basic set of solutions
+	* @return - A Individual array
+	*/
+	public Individual[] get_solution_set(){
 		return solution_set;
+	}
+	
+	/*
+	* Gets the size of set of solutions
+	* @return - The size of solution_set
+	*/
+	public int get_solution_set_size(){
+		return solution_set.length;
+	}
+	
+	/*
+	* Gets the size of the population
+	* @return - population_size
+	*/
+	public int get_population_size(){
+		return population_size;
+	}
+	
+	/*
+	* Add a solution
+	* @param - The new solution to add
+	*/
+	public void add_solution(City[] new_solution){
+		// Create the new Individual
+		Individual new_sol = new Individual(TSPGraph, new_solution, true);
+		
+		// Add the new Individual to the solution set
+		Individual[] new_sol_set = new Individual[solution_set.length + 1];
+		for (int i = 0; i < solution_set.length; i++){
+			new_sol_set[i] = solution_set[i];
+		}
+		new_sol_set[solution_set.length] = new_sol;
+		
+		solution_set = new_sol_set;
+		
+	}
+	
+	/*
+	* Initialise the solution set
+	* Should not generate any errors
+	* @param - An array of the cities in the problem
+	*/
+	private void create_solution_set(City[] cities){
+		City[][] solution_set = new City[population_size][cities.get_num_cities()];
+		
+		for(int i = 0; i < num_solutions; i++){
+			// Create an individual
+			solution_set[i] = new Individual(TSPGraph, cities, false);
+		}
+		
 	}
 }
