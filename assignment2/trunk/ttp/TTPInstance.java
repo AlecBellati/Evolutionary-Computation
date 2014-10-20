2fa27be3ba79f3412b67cca4c33de8da8cc7c299
PrincessPeachey <princesspeachey@hotmail.com>
2014-10-20 21:21:39 +1030

    @PrincessPeachey: testing

--- assignment2/trunk/ttp/TTPInstance.java
+++ assignment2/trunk/ttp/TTPInstance.java
@@ -189,8 +189,8 @@ public class TTPInstance {
          ********************/
         //alec = new Alec(cities, itemsArray, capacityOfKnapsack); alec.getSolution(this);
         //matt = new Matt(cities, itemsArray, minSpeed, maxSpeed, capacityOfKnapsack, rentingRatio); matt.getSolution();
-        sami = new Sami(cities, itemsArray, minSpeed, maxSpeed, capacityOfKnapsack, rentingRatio); sami.getSolution(this);
-        //will = new Will(cities, itemsArray, minSpeed, maxSpeed, capacityOfKnapsack, rentingRatio); runWill(choice);
+        //sami = new Sami(cities, itemsArray, minSpeed, maxSpeed, capacityOfKnapsack, rentingRatio); sami.getSolution(this);
+        will = new Will(cities, itemsArray, minSpeed, maxSpeed, capacityOfKnapsack, rentingRatio); runWill(choice);
             
         getBestSolution(false);
         System.out.println();
@@ -207,8 +207,8 @@ public class TTPInstance {
         String name = "";
         //solution = alec.getBestSolution(); name = "Alec";
         //solution = matt.getBestSolution(); name = "Matt";
-        solution = sami.getBestSolution(); name = "Sami";
-        //solution = will.getBestSolution(); name = "Will";
+        //solution = sami.getBestSolution(); name = "Sami";
+        solution = will.getBestSolution(); name = "Will";
         
         
         //create file name (calling convention is: <ttpfile>.<algorithmname>.<systemtime>)
